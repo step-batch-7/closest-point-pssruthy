c3b418cf024594652fedadd29357fe616f886b47
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-04-13 20:31:09 +0530

    updated variable names

--- point.c
+++ point.c
@@ -1,22 +1,21 @@
-#include "point.h"
 #include <math.h>
+#include "point.h"
 
 double distance_between_points(Point point_A, Point point_B){
-  int x_pow = pow(point_A.x - point_B.x, 2);
-  int y_pow = pow(point_A.y - point_B.y, 2);
-  double distance = sqrt(x_pow + y_pow);
-  return distance;
+  int x_range_pow = pow(point_A.x - point_B.x, 2);
+  int y_range_pow = pow(point_A.y - point_B.y, 2);
+  return sqrt(x_range_pow + y_range_pow);
 }
 
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location) {
-  double shortest_distance = distance_between_points(current_location, food_points[0]);
+  double previous_distance = distance_between_points(current_location, food_points[0]);
   *closest_food_location = food_points[0];
 
   for(int index = 1; index < points_length; index++){
-    double distance_to_corrent_food = distance_between_points(current_location, food_points[index]);
-    if(distance_to_corrent_food < shortest_distance){
-      shortest_distance = distance_to_corrent_food;
+    double corrent_distance = distance_between_points(current_location, food_points[index]);
+    if(corrent_distance < previous_distance){
+      previous_distance = corrent_distance;
       *closest_food_location = food_points[index];
     }
   }
